4094411f1983a015e2eeb501c35a2992e8e8efc7
Platina1337 <116428617+Platina1337@users.noreply.github.com>
2024-06-18 18:56:13 +0300

    Update Custom function.sql

--- Mydb/Custom function.sql
+++ Mydb/Custom function.sql
@@ -1,7 +1,7 @@
 -- Пользовательская функция для получения общей суммы заказа по его идентификатору. -- 
 DELIMITER //
 
-CREATE DEFINER=`root`@`localhost` FUNCTION `GetOrderTotal`(order_id INT) RETURNS decimal(10,2)
+CREATE FUNCTION `GetOrderTotal`(p_order_id INT) RETURNS decimal(10,2)
     DETERMINISTIC
 BEGIN
     DECLARE total DECIMAL(10,2) DEFAULT 0.00;
@@ -9,9 +9,10 @@ BEGIN
     -- Подсчитываем общую сумму заказа
     SELECT SUM(price * quantity) INTO total
     FROM order_detail
-    WHERE order_id = order_id;
+    WHERE order_id = p_order_id;
 
     RETURN total;
 END //
 
 DELIMITER ;
+
